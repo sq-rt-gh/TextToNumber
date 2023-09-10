5d23eede4e33e12d28bf697aaf91a64f371251fe
Олег Гуськов <whitetiger.guskov@ya.ru>
2023-09-10 23:50:22 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Homework1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.labelAnswer = new System.Windows.Forms.Label();
+            this.textBoxErrors = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.labelError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxInput.Location = new System.Drawing.Point(32, 50);
+            this.textBoxInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(500, 34);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.TextBoxInput_TextChanged);
+            // 
+            // labelAnswer
+            // 
+            this.labelAnswer.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelAnswer.Location = new System.Drawing.Point(192, 140);
+            this.labelAnswer.Name = "labelAnswer";
+            this.labelAnswer.Size = new System.Drawing.Size(300, 100);
+            this.labelAnswer.TabIndex = 4;
+            this.labelAnswer.Text = "000";
+            this.labelAnswer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelAnswer.Visible = false;
+            // 
+            // textBoxErrors
+            // 
+            this.textBoxErrors.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxErrors.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxErrors.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxErrors.Location = new System.Drawing.Point(56, 140);
+            this.textBoxErrors.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxErrors.Multiline = true;
+            this.textBoxErrors.Name = "textBoxErrors";
+            this.textBoxErrors.ReadOnly = true;
+            this.textBoxErrors.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxErrors.Size = new System.Drawing.Size(580, 260);
+            this.textBoxErrors.TabIndex = 2;
+            this.textBoxErrors.TabStop = false;
+            this.textBoxErrors.Visible = false;
+            // 
+            // button
+            // 
+            this.button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button.Enabled = false;
+            this.button.Location = new System.Drawing.Point(539, 46);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(111, 34);
+            this.button.TabIndex = 1;
+            this.button.Text = "Translate";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelError.Location = new System.Drawing.Point(27, 110);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(70, 25);
+            this.labelError.TabIndex = 3;
+            this.labelError.Text = "Errors:";
+            this.labelError.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 453);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.labelAnswer);
+            this.Controls.Add(this.textBoxErrors);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MinimumSize = new System.Drawing.Size(500, 400);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text to digits";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label labelAnswer;
+        private System.Windows.Forms.TextBox textBoxErrors;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label labelError;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Homework1
+{
+    public partial class Form1 : Form
+    {
+        enum NumberType {Zero, Digit, Teen, Dozen, Hundred };
+
+        readonly Dictionary<string, int> digits = new Dictionary<string, int>(9)
+        {
+            { "one", 1},
+            { "two", 2},
+            { "three", 3},
+            { "four", 4},
+            { "five", 5},
+            { "six", 6},
+            { "seven", 7},
+            { "eight", 8},
+            { "nine", 9}
+        };
+
+        readonly Dictionary<string, int> teens = new Dictionary<string, int>(10)
+        {
+            ["ten"] = 10,
+            ["eleven"] = 11,
+            ["twelve"] = 12,
+            ["thirteen"] = 13,
+            ["fourteen"] = 14,
+            ["fifteen"] = 15,
+            ["sixteen"] = 16,
+            ["seventeen"] = 17,
+            ["eighteen"] = 18,
+            ["nineteen"] = 19
+        };
+
+        readonly Dictionary<string, int> dozens = new Dictionary<string, int>(8)
+        {
+            ["twenty"] = 20,
+            ["thirty"] = 30,
+            ["forty"] = 40,
+            ["fifty"] = 50,
+            ["sixty"] = 60,
+            ["seventy"] = 70,
+            ["eighty"] = 80,
+            ["ninety"] = 90
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            labelError.Visible = false;
+            textBoxErrors.Visible = false;
+            labelAnswer.Visible = false;
+            textBoxErrors.Text = string.Empty;
+            labelAnswer.Text = string.Empty;
+
+            string s = textBoxInput.Text.Trim().ToLower();
+            while (s.Contains("  "))
+                s = s.Replace("  ", " ");
+
+            string[] words = s.Split();
+
+            if (words.Length == 0) return;
+
+            foreach (string word in words)
+            {
+                if (!(word == "hundred" || word == "zero" || digits.ContainsKey(word) 
+                    || teens.ContainsKey(word) || dozens.ContainsKey(word)))
+                {
+                    textBoxErrors.AppendText($"Орфографическая ошибка в слове '{word}'. \r\n");
+                }
+            }
+            
+            if (textBoxErrors.Text != string.Empty)
+            {
+                labelError.Visible = true;
+                textBoxErrors.Visible = true;
+                return;
+            }
+
+            int cnt = 0;
+            NumberType curr = Typeof(words[0]);
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                NumberType next = Typeof(words[i + 1]);
+
+                switch (curr) 
+                {
+                    case NumberType.Zero:
+                        textBoxErrors.AppendText($"Синтаксическая ошибка: после 'zero' не может ничего стоять. \r\n");
+                        break;
+
+                    case NumberType.Hundred:
+                        cnt++;
+                        if (next == NumberType.Zero)
+                        {
+                            textBoxErrors.AppendText($"Синтаксическая ошибка: 'zero' не может стоять после 'hundred'. \r\n");
+                        }
+                        else if (next == NumberType.Hundred)
+                        {
+                            textBoxErrors.AppendText($"Синтаксическая ошибка: повторение слова 'hundred'. \r\n");
+                        }
+                        break;
+
+                    case NumberType.Digit:
+                        if (next == NumberType.Zero)
+                        {
+                            textBoxErrors.AppendText($"Синтаксическая ошибка: 'zero' не может стоять после числа еденичного формата. \r\n");
+                        }
+                        else if (next == NumberType.Digit)
+                        {
+                            textBoxErrors.AppendText($"Синтаксическая ошибка: числа еденичного формата не могут стоять рядом. \r\n");
+                        }
+                        else if (next == NumberType.Teen)
+                        {
+                            textBoxErrors.AppendText($"Синтаксическая ошибка: после числа еденичного формата не может идти число формата 10-19. \r\n");
+                        }
+                        else if (next == NumberType.Dozen)
+                        {
+                            textBoxErrors.AppendText($"Синтаксическая ошибка: после числа еденичного формата не может идти число десятичного формата. \r\n");
+                        }
+                        break;
+
+                    case NumberType.Teen:
+                        if (next == NumberType.Zero)
+                        {
+                            textBoxErrors.AppendText($"Синтаксическая ошибка: 'zero' не может стоять после числа формата 10-19. \r\n");
+                        }
+                        else if (next == NumberType.Teen)
+                        {
+                            textBoxErrors.AppendText($"Синтаксическая ошибка: числа формата 10-19 не могут стоять рядом. \r\n");
+                        }
+                        else if (next == NumberType.Digit)
+                        {
+                            textBoxErrors.AppendText($"Синтаксическая ошибка: после числа формата 10-19 не может идти число еденичного формата. \r\n");
+                        }
+                        else if (next == NumberType.Dozen)
+                        {
+                            textBoxErrors.AppendText($"Синтаксическая ошибка: после числа формата 10-19 не может идти число десятичного формата. \r\n");
+                        }
+                        
+                        for (int j = i + 1; j < words.Length; j++)
+                        {
+                            if (words[j] == "hundred")
+                            {
+                                textBoxErrors.AppendText($"Синтаксическая ошибка: число формата 10-19 не может стоять перед 'hundred'. \r\n");
+                                break;
+                            }
+                        }
+                        break;
+
+                    case NumberType.Dozen:
+                        if (next == NumberType.Zero)
+                        {
+                            textBoxErrors.AppendText($"Синтаксическая ошибка: 'zero' не может стоять после числа десятичного формата. \r\n");
+                        }
+                        else if (next == NumberType.Dozen)
+                        {
+                            textBoxErrors.AppendText($"Синтаксическая ошибка: числа десятичного формата не могут стоять рядом. \r\n");
+                        }
+                        else if (next == NumberType.Teen)
+                        {
+                            textBoxErrors.AppendText($"Синтаксическая ошибка: после числа десятичного формата не может идти число формата 10-19. \r\n");
+                        }
+                        for (int j = i + 1; j < words.Length; j++)
+                        {
+                            if (words[j] == "hundred")
+                            {
+                                textBoxErrors.AppendText($"Синтаксическая ошибка: число десятичного формата не может стоять перед 'hundred'. \r\n");
+                                break;
+                            }
+                        }
+                        break;
+                }
+
+                curr = next;
+            }
+            if (curr == NumberType.Hundred) 
+                cnt++;
+            if (cnt>1 && !textBoxErrors.Text.Contains("повторение слова 'hundred'."))
+                textBoxErrors.AppendText("Синтаксическая ошибка: повторение слова 'hundred'. \r\n");
+
+            if (textBoxErrors.Text != string.Empty)
+            {
+                labelError.Visible = true;
+                textBoxErrors.Visible = true;
+                return;
+            }
+
+            int ans = 0;
+            foreach (string word in words)
+            {
+                switch (Typeof(word))
+                {
+                    case NumberType.Zero:
+                        ans = 0;
+                        break;
+
+                    case NumberType.Digit:
+                        ans += digits[word];
+                        break;
+
+                    case NumberType.Teen:
+                        ans += teens[word];
+                        break;
+
+                    case NumberType.Dozen:
+                        ans += dozens[word];
+                        break;
+
+                    case NumberType.Hundred:
+                        ans *= 100;
+                        break;
+
+                }
+            }
+            labelAnswer.Text = ans.ToString(); 
+            labelAnswer.Visible = true;
+        }
+
+        private NumberType Typeof(string word)
+        {
+            if (word == "zero")
+                return NumberType.Zero;
+            if (word == "hundred")
+                return NumberType.Hundred;
+            if (digits.ContainsKey(word))
+                return NumberType.Digit;
+            if (dozens.ContainsKey(word))
+                return NumberType.Dozen;
+            
+            return NumberType.Teen;
+        }
+
+        private void TextBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            button.Enabled = textBoxInput.Text.Trim().Length != 0;
+        }
+    }
+}
